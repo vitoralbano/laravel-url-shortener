3b83d3af2fe215a6fdd52a94f3120eaf6b873745
Vitor Albano <vitoralbano@gmail.com>
2020-10-04 16:44:35 -0300

    scafold jetstream livewire

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function() {
+    Route::get('/dashboard', function() { return view('dashboard'); })
+        ->name('dashboard');
+
+    Route::resource('urls', 'ShortUrls');
+});
